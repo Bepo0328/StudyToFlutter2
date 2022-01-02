6502b17c21c5149e7751207b0fed036f57e862f9
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-01-02 22:26:49 +0900

    StudyToFlutter2: Widget

---
+++ Flutter 자료(마크다운)/Flutter2 - Widget(위젯).md
@@ -0,0 +1,33 @@
+Flutter2 - Widget(위젯)
+========
+
+# 1. Widget(위젯)
+	Flutter에서 기본 디자인 요소를 위젯이라고 부릅니다
+    Flutter는 위젯이 쌓여 앱을 구성합니다
+    
+    - MaterialApp 클래스
+    머터리얼 디자인을 사용하는 어플리케이션
+    플러터 앱의 기본 구조
+    
+    - Scaffold 클래스
+    플러터 앱 페이지 기초 구조
+    AppBar, FAB(Floating Action Button),
+    BottomNavigationBar
+    
+    - Text 위젯
+    글자를 표현하는 위젯
+    
+    - Icon 위젯
+    아이콘을 표현하는 위젯
+    
+    - Image 위젯
+    이미지를 표현하는 위젯
+    
+    - Container 위젯
+    다른 위젯을 감싸는 위젯
+    
+    - TextButton 위젯
+    클릭 가능한 텍스트 버튼
+    
+    - IconButton 위젯
+    클릭 가능한 아이콘 버튼
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,77 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  double bmi = 27.1;
-
-  if(bmi > 30) {
-    print("초고도 비만");
-  } else if(bmi > 25) {
-    print("비만");
-  } else {
-    print("정상입니다.");
-  }
-
-  for(int i = 0; i < 10; i++) {
-    print(i);
-  }
-
-  List<String> students = ["영수", "지영", "찬형", "지민"];
-  for(var name in students) {
-    print(name);
-  }
-
-  int i = 0;
-  while(i < 10) {
-    print(i);
-    i++;
-  }
-
-  print("5단 출력");
-  for(int i = 1; i < 10; i++) {
-    print("5 * $i = ${5 * i}");
-  }
-
-  i = 0;
-  print("5단 출력");
-  while(i < 10) {
-    print("5 * $i = ${5 * i}");
-    i++;
-  }
-
-  print(sum(5, 7));
-}
-
-int sum (int x, int y) {
-  return x + y;
-}
-
-class Student {
-  int? number;
-  String? name;
-
-  void printStudents() {
-    print("번호 $number 이름: $name");
-  }
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -81,16 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -102,55 +32,33 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'My First App',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const Icon(Icons.add),
+            Container(
+              child: const Text('컨테이너'),
+              color: Colors.redAccent,
+            ),
+            TextButton(
+              onPressed: () {
+                print('텍스트 버튼 눌림');
+              },
+              child: const Text('텍스트 버튼!'),
             ),
           ],
         ),
